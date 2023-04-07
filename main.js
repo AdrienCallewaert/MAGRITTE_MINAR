d487b5907f9f06b5ca66a101c2e9ab3811df56c5
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2023-04-07 15:25:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const start = async() => {
         const mindarThree = new window.MINDAR.IMAGE.MindARThree({
             container: document.body,
-            imageTargetSrc: '.assets/targets/musicband.mind',
+            imageTargetSrc: '.assets/libs//targets/musicband.mind',
         });
         const { renderer, scene, camera } = mindarThree;
 
         const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
         scene.add(light);
 
-        const gltf = await loadGLTF('./assets/models/musicband-raccoon/scene.gltf');
+        const gltf = await loadGLTF('./assets/libs//models/musicband-raccoon/scene.gltf');
         gltf.scene.scale.set(0.1, 0.1, 0.1);
         gltf.scene.position.set(0, -0.4, 0);
 
