2c411eb2a37b5ebadfdd719c90668671c6d6b78a
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2023-04-13 13:48:52 +0200

    up figure

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
         scene.add(light);
 
-        const gltf = await loadGLTF('./assets/applications/assets/models/plane/scene.gltf');
+        const gltf = await loadGLTF('./assets/applications/assets/models/statue/scene.gltf');
         gltf.scene.scale.set(1, 1, 1);
        // gltf.scene.scale.set(0.4, 0.4, 0.4);
         gltf.scene.position.set(0, -0.4, 0);
