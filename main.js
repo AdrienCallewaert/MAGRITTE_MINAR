7b192590665980e31ca58c9b859c964cc7c6960c
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2023-06-16 15:06:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,36 +6,35 @@ document.addEventListener('DOMContentLoaded', () => {
         const mindarThree = new window.MINDAR.IMAGE.MindARThree({
             container: document.body,
             imageTargetSrc: './assets/applications/assets/targets/ehtargets.mind',
-            //imageTargetSrc: './assets/applications/assets/targets/target_eh-card.mind',
         });
         const { renderer, scene, camera } = mindarThree;
 
         const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
         scene.add(light);
 
-       // const gltf = await loadGLTF('./assets/applications/assets/models/melon/scene.gltf');
-
         const gltf = await loadGLTF('./assets/applications/assets/models/ehman/GLTF/scene.gltf');
         gltf.scene.scale.set(0.8, 0.8, 0.8);
-       // gltf.scene.scale.set(0.4, 0.4, 0.4);
         gltf.scene.position.set(0, 0, 0);
 
+        raccoon.scene.scale.set(0.1, 0.1, 0.1);
+        raccoon.scene.position.set(0, -0.4, 0);
+
         const anchor = mindarThree.addAnchor(0);
-        anchor.group.add(gltf.scene);
+        anchor.group.add(raccoon.scene);
 
-        // const mixer = new THREE.AnimationMixer(gltf.scene);
-        // const action = mixer.clipAction(gltf.animations[0]);
-        // action.play();
-
-        const clock = new THREE.Clock();
+        anchor.onTargetFound = () => {
+        console.log("on target found");
+        }
+        anchor.onTargetLost = () => {
+        console.log("on target lost");
+        }
 
         await mindarThree.start();
         renderer.setAnimationLoop(() => {
-            const delta = clock.getDelta();
-            gltf.scene.rotation.set(90, gltf.scene.rotation.y + delta, 0);
-            //mixer.update(delta);
-            renderer.render(scene, camera);
-        });
-    }
-    start();
+        renderer.render(scene, camera);
+    });
+  }
+  start();
 });
+
+
