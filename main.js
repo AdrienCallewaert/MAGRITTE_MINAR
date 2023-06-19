f3b2d9b42e1faaf38e93245b832b0d7a0044834c
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2023-06-19 14:19:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -14,14 +14,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const start = async() => {
         const mindarThree = new window.MINDAR.IMAGE.MindARThree({
             container: document.body,
-            imageTargetSrc: './assets/applications/assets/targets/ehtargets.mind',
+            imageTargetSrc: "./assets/applications/assets/targets/ehtargets.mind",
         });
         const { renderer, scene, camera } = mindarThree;
 
         const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
         scene.add(light);
 
-        const gltf = await loadGLTF('./assets/applications/assets/models/ehman/GLTF/scene.gltf');
+        const gltf = await loadGLTF("./assets/applications/assets/models/ehman/GLTF/scene.gltf");
         gltf.scene.scale.set(0.8, 0.8, 0.8);
         gltf.scene.position.set(0, 0, 0);
         //gltf.scene.position.set(0, -1, 0); descend
